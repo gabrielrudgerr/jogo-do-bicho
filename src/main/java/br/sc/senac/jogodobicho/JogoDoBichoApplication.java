c58d4d07f20442de9c625427aa9132ab5da4053e
Gabriel Rüdger <60497549+gabrielrudgerr@users.noreply.github.com>
2020-09-30 10:19:49 -0300

    Versão inicial

---
+++ src/main/java/br/sc/senac/jogodobicho/JogoDoBichoApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.jogodobicho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoDoBichoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoDoBichoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sc/senac/jogodobicho/JogoDoBichoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senac.jogodobicho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoDoBichoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
